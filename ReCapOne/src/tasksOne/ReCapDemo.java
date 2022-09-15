185f2417e6f6e2f1da97dc26c9fafb8070904814
Ali Kaygusuz <ali@kodcular.net>
2022-09-16 00:39:47 +0300

    Bug fixed | Pick the biggest number.

--- ReCapOne/src/tasksOne/ReCapDemo.java
+++ ReCapOne/src/tasksOne/ReCapDemo.java
@@ -4,9 +4,9 @@ public class ReCapDemo {
 
 	public static void main(String[] args) {
 		
-		int numOne = 23;
-		int numTwo = 15;
-		int numThree = 29;
+		int numOne = 20;
+		int numTwo = 21;
+		int numThree = 20;
 		int theBiggestNum = 0;
 		
 		if (numOne > numTwo && numOne > numThree) {
@@ -16,10 +16,12 @@ public class ReCapDemo {
 			theBiggestNum = numTwo;
 			System.out.println("En büyük sayı:  "  + theBiggestNum);
 			
-		} else {
+		} else if(numThree > numOne && numThree > numTwo) {
 			theBiggestNum = numThree;
 			System.out.println("En büyük sayı:  "  + theBiggestNum);
 			
+		}else {
+			System.out.println("Bütün sayılar eşittir.");
 		}
 	}
 
