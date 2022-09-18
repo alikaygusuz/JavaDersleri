812e7b4459c91ffce83fefcf03d2127e7b53abd3
Ali Kaygusuz <ali@kodcular.net>
2022-09-18 21:51:51 +0300

    For loop example created.

---
+++ loopDemo/src/loopDemo/main.java
@@ -0,0 +1,12 @@
+package loopDemo;
+
+public class main {
+
+	public static void main(String[] args) {
+		for (int i = 0; i <= 10; i++) {
+			System.out.println(i);
+		}
+		System.out.println("Döngü bitti.");
+	}
+
+}
