71a216478368695362957f35ba07a81d236dbd78
Ali Kaygusuz <ali@kodcular.net>
2022-09-18 22:47:44 +0300

    reCapDemo2 completed.

---
+++ reCapDemo2/src/reCapDemo2/Main.java
@@ -0,0 +1,29 @@
+package reCapDemo2;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		double numbers[] = {1.2, 2.3, 4.8, 6.7, 2.5};
+		double total = 0;
+		double max = numbers[0];
+		
+		for (double maxNumber : numbers) {
+			double  isItPositive = max - maxNumber;
+			if (isItPositive < 0) {
+				max = maxNumber;
+				}
+
+			}
+		
+		for (double number : numbers) {
+			total = total + number;
+			System.out.println(number);
+		}
+		
+		System.out.println("Girdiğiniz sayıların toplamı:" + total);
+		System.out.println("En büyük sayı= " + max);
+		
+	}
+
+}
