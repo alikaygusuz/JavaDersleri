4a6f94d4a22e60d821afd1d90422d98eb788d84c
Ali Kaygusuz <ali@kodcular.net>
2022-09-18 22:00:00 +0300

    For and while loop examples complated.

--- loopDemo/src/loopDemo/main.java
+++ loopDemo/src/loopDemo/main.java
@@ -3,10 +3,20 @@ package loopDemo;
 public class main {
 
 	public static void main(String[] args) {
+		
+		//For Loop
 		for (int i = 0; i <= 10; i++) {
 			System.out.println(i);
 		}
-		System.out.println("Döngü bitti.");
+		System.out.println("For döngüsü bitti.");
+		
+		//While Loop
+		int i = 1;
+		while (i<=10) {
+			System.out.println(i);
+			i++;
+		}
+		System.out.println("While döngüsü bitti.");
 	}
 
 }
